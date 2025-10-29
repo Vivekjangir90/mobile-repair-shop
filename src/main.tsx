6702669544c0e9978ed5d6b9f2d4e0d6fe7b7f8a
vivekjangir90 <vivekjangir90@gmail.com>
2025-10-29 10:29:22 +0530

    Update commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx' // <--- यहाँ `.tsx` जोड़ें
+import App from './app.tsx' // <--- यहाँ `.tsx` जोड़ें
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
